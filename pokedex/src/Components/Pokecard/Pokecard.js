d5294abca977d488a05c51ff1973eb4db67481cf
InesBarone <minesbarone@gmail.com>
2022-08-11 16:46:48 -0300

    third commit

--- pokedex/src/Components/Pokecard/Pokecard.js
+++ pokedex/src/Components/Pokecard/Pokecard.js
@@ -6,88 +6,46 @@ import { Link } from "react-router-dom";
 export default function Pokecard({ text }) {
   // text es el texto que se cambia con el Input, entonces si no hay nada escrito
   // queremos que se devuelva toda la lista de pokemones como hasta ahora:
+  let string = text;
+  let regex = new RegExp(string, "gi");
+  let filtered = Pokeinfo.filter(function (pokemon) {
+    return pokemon.name.match(regex);
+  });
 
-  if (text === "") {
-    return (
-      <div className="pokecard-container">
-        {Pokeinfo.map((pokemon) => {
-          const borderColor = {
-            border: `2px solid ${pokemon.primaryColor}`,
-          };
-          const colorId = {
-            color: `${pokemon.primaryColor}`,
-          };
-          const colorName = {
-            backgroundColor: `${pokemon.primaryColor}`,
-          };
+  // Una ves obtenido el filtrado (en este caso seria la constante filtered) podremos mostrarlos:
+  return (
+    <div className="pokecard-container">
+      {filtered.map((pokemon) => {
+        const borderColor = {
+          border: `2px solid ${pokemon.primaryColor}`,
+        };
+        const colorId = {
+          color: `${pokemon.primaryColor}`,
+        };
+        const colorName = {
+          backgroundColor: `${pokemon.primaryColor}`,
+        };
 
-          return (
-            <div className="card" style={borderColor} key={pokemon.name}>
-              <Link to={`/pokemon/${pokemon.name}`}>
-                <div className="id-card" style={colorId}>
-                  {pokemon.id}
-                </div>
-                <div className="img-card-container">
-                  <img
-                    src={pokemon.img}
-                    className="img-card"
-                    alt="pokemon-photo"
-                  />
-                </div>
-                <div className="name-card" style={colorName}>
-                  {pokemon.name}
-                </div>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-  // En cambio si text es distinto de "" entonces queremos que se filtren los pokemones cuyo nombre
-  // corresponda con el de text (esta parte la hice igual que el ejercicio 3)
-  else {
-    let string = text;
-    let regex = new RegExp(string, "gi");
-    let filtered = Pokeinfo.filter(function (pokemon) {
-      return pokemon.name.match(regex);
-    });
-
-    // Una ves obtenido el filtrado (en este caso seria la constante filtered) podremos mostrarlos:
-    return (
-      <div className="pokecard-container">
-        {filtered.map((pokemon) => {
-          const borderColor = {
-            border: `2px solid ${pokemon.primaryColor}`,
-          };
-          const colorId = {
-            color: `${pokemon.primaryColor}`,
-          };
-          const colorName = {
-            backgroundColor: `${pokemon.primaryColor}`,
-          };
-
-          return (
-            <div className="card" style={borderColor} key={pokemon.name}>
-              <Link to={`/pokemon/${pokemon.name}`}>
-                <div className="id-card" style={colorId}>
-                  {pokemon.id}
-                </div>
-                <div className="img-card-container">
-                  <img
-                    src={pokemon.img}
-                    className="img-card"
-                    alt="pokemon-photo"
-                  />
-                </div>
-                <div className="name-card" style={colorName}>
-                  {pokemon.name}
-                </div>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+        return (
+          <div className="card" style={borderColor} key={pokemon.name}>
+            <Link to={`/pokemon/${pokemon.name}`}>
+              <div className="id-card" style={colorId}>
+                {pokemon.id}
+              </div>
+              <div className="img-card-container">
+                <img
+                  src={pokemon.img}
+                  className="img-card"
+                  alt="pokemon-photo"
+                />
+              </div>
+              <div className="name-card" style={colorName}>
+                {pokemon.name}
+              </div>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
